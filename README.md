2ff85cb397b31a0e582605f8d1a15dcf94283d8a
lisnote <lisnote@lisnote.com>
2024-01-12 17:48:57 +0800

    feat: 增加功能并完善使用文档

--- README.md
+++ README.md
@@ -1,3 +1,189 @@
 # Tree Factory
 
 Powerful functions for various tree operations.
+
+## Installation
+
+### Using npm
+
+```bash
+npm install tree-factory
+```
+
+```javascript
+import * as treeFactory from 'tree-factory';
+```
+
+```javascript
+const treeFactory = require('tree-factory');
+```
+
+### Using CDN
+
+```html
+<script src="https://unpkg.com/tree-factory/dist/index.umd.cjs"></script>
+```
+
+You can find the library on `window.treeFactory`.
+
+## Usage
+
+### treeFromList
+
+Create a tree structure data from a flat list.
+
+- Arguments
+
+  source - The flat list of data to convert into a tree.
+
+  [options] - Customization options.
+
+  [options.id] - The key representing the unique identifier for each node.
+
+  [options.pid] - The key representing the parent identifier for each node.
+
+- Returns
+
+  The root nodes of the resulting tree structure.
+
+- Example
+
+  ```javascript
+  treeFromList([{ id: 0, pid: 1 }, { id: 1 }]);
+  treeFromList([{ roleId: 0, parentId: 2 }, { roleId: 2 }], {
+    id: 'roleId',
+    pid: 'parentId',
+  });
+  ```
+
+### treeToList
+
+Pushes all nodes from the provided tree to the specified list or creates a new list.
+
+- Arguments
+
+  tree - The tree from which to extract nodes.
+
+  [options] - Customization options.
+
+  [options.children] - The key representing children nodes in each tree node.
+
+  [options.list] - The list to which the nodes are pushed.
+
+- Returns
+
+  The flat list containing all nodes from the tree.
+
+- Example
+
+  ```javascript
+  treeToList([{ value: 0, children: [{ value: 1 }] }]);
+  treeToList([{ value: 0, subTree: [{ value: 1 }] }], {
+    children: 'subTree',
+    list: [],
+  });
+  ```
+
+### treeCleaner
+
+Removes nodes from the tree that do not satisfy the predicate function.
+
+- Arguments
+
+  tree - The tree to be cleaned.
+
+  predicate - The function used to determine if a node should be removed.
+
+  [options] - Customization options.
+
+  [options.children] - The key representing children nodes in each tree node.
+
+  [options.parent] - The parent node of the current node being processed.
+
+- Returns
+
+  The cleaned tree.
+
+- Example
+
+  ```javascript
+  treeCleaner(
+    [{ value: 0, children: [{ value: 1 }, { value: 2 }] }, { value: 2 }],
+    (node, parent) => node.value > 1 && parent,
+  );
+  treeCleaner(
+    [{ value: 0, subTree: [{ value: 1 }, { value: 2 }] }],
+    (node) => node.value > 1,
+    { children: 'subTree' },
+  );
+  ```
+
+### treeTraverse
+
+Executes a provided function once for each node in a tree.
+
+- Arguments
+
+  tree - The tree to be traversed.
+
+  handle - Function to execute for each node.
+
+  [options] - Customization options.
+
+  [options.children] - The key representing children nodes in each tree node.
+
+  [options.parent] - The parent node of the current node being processed.
+
+  [options.isPreorder] - Specifies whether to use preorder traversal (true) or postorder traversal (false)
+
+- Returns
+
+  void
+
+- Example
+
+  ```javascript
+  treeTraverse(
+    [{ value: 0, children: [{ value: 1 }, { value: 2 }] }, { value: 2 }],
+    (node, parent) => (node.value += parent.value ?? 0),
+  );
+  treeTraverse(
+    [{ value: 0, subTree: [{ value: 1 }, { value: 2 }] }],
+    (node) => node.value++,
+    { children: 'subTree', isPreorder: true },
+  );
+  ```
+
+### treeFind
+
+Finds the first node in the tree that satisfies the predicate function.
+
+- Arguments
+
+  tree - The tree to be searched.
+
+  predicate - The function used to determine if a node matches the search criteria.
+
+  [options] - An object containing optional parameters.
+
+  [options.children] - The property name representing the children nodes in each tree node.
+
+  [options.parent] - The parent node of the current node being processed.
+
+- Returns
+
+  The first node that matches the search criteria, or undefined if no match is found.
+
+- Example
+
+  ```javascript
+  treeFind(
+    [{ value: 0, children: [{ value: 1 }, { value: 2 }] }, { value: 2 }],
+    (node, parent) => node.value > 1 && parent,
+  );
+  treeFind(
+    [{ value: 0, subTree: [{ value: 1 }, { value: 2 }] }],
+    (node) => node.value > 1,
+    { children: 'subTree' },
+  );
+  ```
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,154 @@
-export function add(a: number, b: number) {
-  return a + b;
+/*! type-guardian | MIT License | © 2024 lisnote */
+
+export type StandardTree<T extends any = any> = {
+  children: StandardTree[];
+  data: T;
+  parent?: StandardTree;
+};
+
+/**
+ * Create a tree structure data from a flat list.
+ *
+ * @param source - The flat list of data to convert into a tree.
+ * @param [options] - Customization options.
+ * @param [options.id] - The key representing the unique identifier for each node.
+ * @param [options.pid] - The key representing the parent identifier for each node.
+ * @returns The root nodes of the resulting tree structure.
+ */
+export function treeFromList<T extends any[] = any>(
+  source: T,
+  { id = 'id', pid = 'pid' }: { id?: string; pid?: string } = {},
+): StandardTree<T[number]>[] {
+  const root: StandardTree<T>[] = [];
+  const idNodeMap: Record<string, StandardTree<T[number]>> = {};
+  source.forEach((v) => (idNodeMap[v[id]] = { children: [], data: v }));
+  Object.values(idNodeMap).forEach((v) => {
+    const parent = idNodeMap[v.data[pid]];
+    if (parent) {
+      parent.children.push(v);
+      v.parent = parent;
+    } else {
+      root.push(v);
+    }
+  });
+  return root;
+}
+
+/**
+ * Pushes all nodes from the provided tree to the specified list or creates a new list.
+ *
+ * @param tree - The tree from which to extract nodes.
+ * @param [options] - Customization options.
+ * @param [options.children] - The key representing children nodes in each tree node.
+ * @param [options.list] - The list to which the nodes are pushed.
+ * @return The flat list containing all nodes from the tree.
+ */
+export function treeToList<T extends any[]>(
+  tree: T,
+  {
+    children = 'children',
+    list = [] as any,
+  }: { children?: keyof T[number]; list?: T } = {},
+): T {
+  tree.forEach((node) => {
+    list.push(node);
+    const childNodes = node[children];
+    if (childNodes?.length) treeToList(childNodes, { children, list });
+  });
+  return list;
+}
+
+/**
+ * Removes nodes from the tree that do not satisfy the predicate function.
+ *
+ * @param tree - The tree to be cleaned.
+ * @param predicate - The function used to determine if a node should be removed.
+ * @param [options] - Customization options.
+ * @param [options.children] - The key representing children nodes in each tree node.
+ * @param [options.parent] - The parent node of the current node being processed.
+ * @return The cleaned tree.
+ */
+export function treeCleaner<T extends any[]>(
+  tree: T,
+  predicate: (node: T[number], parent?: T[number]) => boolean,
+  {
+    children = 'children',
+    parent,
+  }: { children?: keyof T[number]; parent?: T[number] } = {},
+): T {
+  return tree.reduceRight((pre, now, index) => {
+    if (predicate(now, parent)) {
+      return pre;
+    }
+    if (now[children]) {
+      treeCleaner(now[children], predicate, { children, parent: now });
+    }
+    if ((now[children]?.length ?? 0) < 1) {
+      tree.splice(index, 1);
+    }
+    return pre;
+  }, tree);
+}
+
+/**
+ * Executes a provided function once for each node in a tree.
+ *
+ * @param tree - The tree to be traversed.
+ * @param handle - Function to execute for each node.
+ * @param [options] - Customization options.
+ * @param [options.children] - The key representing children nodes in each tree node.
+ * @param [options.parent] - The parent node of the current node being processed.
+ * @param [options.isPreorder] - Specifies whether to use preorder traversal (true) or postorder traversal (false)
+ */
+export function treeTraverse<T extends any[]>(
+  tree: T,
+  handle: (node: T[number], parent?: T[number]) => void,
+  {
+    children = 'children',
+    parent,
+    isPreorder = false,
+  }: {
+    children?: keyof T[number];
+    parent?: T[number];
+    isPreorder?: boolean;
+  } = {},
+) {
+  tree.forEach((node) => {
+    if (isPreorder) handle(node, parent);
+    const childNodes = node[children];
+    if (childNodes?.length) {
+      treeTraverse(childNodes, handle, { children, parent: node, isPreorder });
+    }
+    if (!isPreorder) handle(node, parent);
+  });
+}
+
+/**
+ * Finds the first node in the tree that satisfies the predicate function.
+ *
+ * @param tree - The tree to be searched.
+ * @param predicate - The function used to determine if a node matches the search criteria.
+ * @param [options] - An object containing optional parameters.
+ * @param [options.children] - The property name representing the children nodes in each tree node.
+ * @param [options.parent] - The parent node of the current node being processed.
+ * @return The first node that matches the search criteria, or undefined if no match is found.
+ */
+export function treeFind<T extends any[]>(
+  tree: T,
+  predicate: (node: T[number], parent?: T[number]) => boolean,
+  {
+    children = 'children',
+    parent,
+  }: { children?: keyof T[number]; parent?: T[number] } = {},
+): T[number] | void {
+  for (const child of tree) {
+    if (child[children]) {
+      const target = treeFind(child[children], predicate, {
+        children,
+        parent: child,
+      });
+      if (target) return target;
+    }
+    if (predicate(child, parent)) return child;
+  }
 }
