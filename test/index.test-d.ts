e8a181fa96fe779125a5a40ce76c41cd76f79cdd
lisnote <lisnote@lisnote.com>
2024-01-14 11:26:52 +0800

    feat: 增加功能并完善使用文档

---
+++ test/index.test-d.ts
@@ -0,0 +1,15 @@
+import { expectTypeOf, test } from 'vitest';
+import { type StandardTree, treeFromList } from '../src/index.ts';
+
+const source: { id: number; value: number; pid?: number }[] = [
+  { id: 1, value: 1 },
+  { id: 0, value: 0, pid: 3 },
+  { id: 3, value: 3 },
+  { id: 2, value: 2, pid: 1 },
+  { id: 4, value: 4, pid: 1 },
+];
+const tree = treeFromList(source);
+
+test('StandardTree', () => {
+  expectTypeOf(tree).toMatchTypeOf<StandardTree<(typeof source)[number]>[]>();
+});
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,6 +1,43 @@
 import { expect, test } from 'vitest';
-import { add } from '@/index';
+import { cloneDeep } from 'lodash-es';
+import {
+  treeCleaner,
+  treeFind,
+  treeFromList,
+  treeToList,
+  treeTraverse,
+} from '../src/index.ts';
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(add(1, 2)).toBe(3);
+const source = [
+  { id: 1, value: 1 },
+  { id: 0, value: 0, pid: 3 },
+  { id: 3, value: 3 },
+  { id: 2, value: 2, pid: 1 },
+  { id: 4, value: 4, pid: 1 },
+];
+const tree = treeFromList(source);
+const cloneTree = () => cloneDeep(tree);
+
+test('treeFromList', () => {
+  expect(tree).toMatchSnapshot();
+});
+
+test('treeToList', () => {
+  expect(treeToList(tree)).toMatchSnapshot();
+});
+
+test('treeCleaner', () => {
+  expect(
+    treeCleaner(cloneTree(), (node) => node.data.value > 4),
+  ).toMatchSnapshot();
+});
+
+test('treeTraverse', () => {
+  const traverseTree = cloneTree();
+  treeTraverse(traverseTree, (node) => node.data.value++);
+  expect(traverseTree).toMatchSnapshot();
+});
+
+test('treeFind', () => {
+  expect(treeFind(tree, (node) => node.data.value > 3)).toMatchSnapshot();
 });
